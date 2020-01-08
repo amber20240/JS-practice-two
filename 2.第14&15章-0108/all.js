65f3f48e3f5498aae4e47559cc2666fe57a7bf7f
Amber <amber20240@gmail.com>
2020-01-08 21:15:40 +0800

    14&15章-0108

---
+++ 2.第14&15章-0108/all.js
@@ -0,0 +1,8 @@
+var el=document.getElementById("l1");
+el.textContent="YA!";
+const el2=document.getElementById("l2");
+el2.textContent=60;
+var drink = "coffee";
+var el3=document.querySelector("#l3");
+el3.textContent=drink;
+alert(15+20);
